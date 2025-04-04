c35509a4f0c458a4d4ea8ae33d2371df19a15a8e
docker-run <maksym.kordiyak@gmail.com>
2025-04-04 23:20:54 +0200

    feat: implement basic route structure

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,5 +2,10 @@ import express from 'express';
 
 export function clientStateRoute() {
   const router = express.Router();
+
+  router.get('/client/state', async (req, res) => {
+    res.status(200).json({});
+  });
+
   return router;
 }
