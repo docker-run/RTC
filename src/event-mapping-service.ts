dc1a456d18ed216825e61cc5e606ca363de18c56
docker-run <maksym.kordiyak@gmail.com>
2025-04-05 00:03:20 +0200

    chore: add constructor methods and create methods for main classes

--- src/event-mapping-service.ts
+++ src/event-mapping-service.ts
@@ -4,3 +4,17 @@ export type EventMappingServiceArgs = {
   fetchMappings: FetchMappingsFn;
   mappingStore: {};
 }
+
+export class EventMappingService {
+  private readonly fetchMappings: FetchMappingsFn;
+  private readonly mappingStore: {};
+
+  public static create(args: EventMappingServiceArgs) {
+    return new this(args);
+  }
+
+  protected constructor({ mappingStore, fetchMappings }: EventMappingServiceArgs) {
+    this.fetchMappings = fetchMappings;
+    this.mappingStore = mappingStore;
+  }
+}
--- src/sport-events-service.ts
+++ src/sport-events-service.ts
@@ -1,13 +1,29 @@
+import { EventMappingService } from "./event-mapping-service";
 import { FetchEventsFn } from "./types";
 
 type SportsEventsServiceArgs = {
   fetchEvents: FetchEventsFn;
-  eventMappingService: {};
+  eventMappingService: EventMappingService;
   eventStore: {};
   historicalEventStore: {};
 }
 
 export class SportsEventsService {
+  private readonly historicalEventStore: {};
+  private readonly eventStore: {};
+  private readonly fetchEvents: FetchEventsFn;
+  private readonly eventMappingService: EventMappingService;
+
+  public static create(args: SportsEventsServiceArgs) {
+    return new this(args);
+  }
+
+  protected constructor({ fetchEvents, eventStore, eventMappingService, historicalEventStore }: SportsEventsServiceArgs) {
+    this.fetchEvents = fetchEvents;
+    this.eventStore = eventStore;
+    this.eventMappingService = eventMappingService;
+    this.historicalEventStore = historicalEventStore;
+  }
 
   public getCurrentEvents() {
 
